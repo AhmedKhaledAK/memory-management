ad0198b4f0dfbc6dda0afdc8ea63ac3844ce96f4
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-12-24 15:41:54 +0200

    took user input

--- main.cpp
+++ main.cpp
@@ -16,6 +16,25 @@ typedef long long ll;
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+    int n; // number of pages
+    string algo; // the simulated algorithm
+    
+    cin >> n;
+    cin >> algo;
+    
+    vector<int> refs; // page references
+    
+    int page;
+    cin >> page;
+    
+    while (page != -1){
+        refs.push_back(page);
+        cin >> page;
+    }
+    
+    cout << endl;
+    
+    lpi(refs.size()) cout << refs[i] << endl;
+    
 	return 0;
 }
