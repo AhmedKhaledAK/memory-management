13e53365cf53db42b3d22854f6d177dc825c22fb
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-12-24 15:30:40 +0200

    added the -maybe useful- defines and includes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <iostream>
+#include<bits/stdc++.h>
+#include <iomanip>
+ 
+#define array_size(a) (sizeof(a)/sizeof(a[0]))
+#define lpi(n) for(int i=0; i<n; i++)
+#define lpiv(v,n) for(int i=v; i<n; i++)
+#define lpj(n) for(int j=0; j<n; j++)
+#define iii pair<int,pair<int,int>>
+#define ii pair<int,int>
+ 
+using namespace std;
+
+typedef long long ll;
 
 int main(int argc, char **argv)
 {
